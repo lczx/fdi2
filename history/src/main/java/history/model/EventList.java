f9c665ec99514673ff07a8f037c462f197ea3c9e
Luca Zanussi <luca.z@outlook.com>
2015-02-10 23:49:40 +0100

    History Channel: Some work on data model

---
+++ history/src/main/java/history/model/EventList.java
@@ -0,0 +1,21 @@
+package history.model;
+
+import java.util.Observable;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class EventList extends Observable {
+
+    SortedSet<HistoryEvent> events = new TreeSet<HistoryEvent>();
+
+    public void addEvents(String name, String year, String location) {
+        getEvents().add(new HistoryEvent(name, Integer.parseInt(year), location));
+        setChanged();
+        notifyObservers();
+    }
+
+    public SortedSet<HistoryEvent> getEvents() {
+        return events;
+    }
+
+}
---
+++ history/src/main/java/history/model/HistoryEvent.java
@@ -0,0 +1,54 @@
+package history.model;
+
+import org.jetbrains.annotations.NotNull;
+
+public class HistoryEvent implements Comparable<HistoryEvent> {
+
+    public static final String DEFAULT_LOCATION = "Altro";
+
+    public static final String[] LOCATIONS = { // Keep sorted!
+            "Altro", "Austria", "Francia", "Germania / Prussia", "Inghilterra", "Italia", "Russia", "Spagna"
+    };
+
+    public static int getLocationID(String location) {
+        for (int i = 0; i < LOCATIONS.length; i++)
+            if (LOCATIONS[i].equalsIgnoreCase(location)) return i;
+        return getLocationID(DEFAULT_LOCATION);
+    }
+
+    String name;
+    int year;
+    int locationID;
+
+    public HistoryEvent(String name, int year, String location) {
+        this.name = name;
+        this.year = year;
+        this.locationID = getLocationID(location);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getLocation() {
+        return LOCATIONS[locationID];
+    }
+
+    @Override
+    public int compareTo(@NotNull HistoryEvent that) {
+        int result = this.year - that.year;
+        if (result != 0) return result;
+
+        result = this.locationID - that.locationID;
+        if (result != 0) return result;
+
+        return this.name.compareTo(that.name);
+    }
+
+    // HashCode implementation seems not to be needed.
+
+}
