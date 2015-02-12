a822b509692df2e4b2846b9be2d90cde74ff6260
Luca Zanussi <luca.z@outlook.com>
2015-02-12 20:17:40 +0100

    Updated dependencies and some experiments with tests

---
+++ benchmark/src/test/java/benchmark/DummyTest.java
@@ -0,0 +1,14 @@
+package benchmark;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class DummyTest {
+
+    @Test
+    public void test01() {
+        assertTrue(1 == 1);
+    }
+
+}
